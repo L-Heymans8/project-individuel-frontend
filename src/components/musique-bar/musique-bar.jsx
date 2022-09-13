bed3866bbfa3d884af64b0375bb7732d13c86589
Lucas Heymans <lucas.heymans8@gmail.com>
2022-09-13 11:30:56 +0200

    Initial commit

---
+++ src/components/musique-bar/musique-bar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './musique-bar.module.css'
+
+const MusiqueBar = () => {
+    return (
+        <div className={style.musiqueBarGlobal}>
+            <div className={style.musiqueBar}>
+            </div>
+        </div>
+    );
+};
+
+export default MusiqueBar;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,20 @@
+
+
+import Cercle from '../../components/cercle-musique/cercle';
+import MusiqueBar from '../../components/musique-bar/musique-bar';
+import Navbar from '../../components/navbar/navbar';
+// import style from './home.module.css';
+import style from './home.module.css'
+
+const Home = () => {
+  return (
+    <div className={style.homeGlobal}>
+      <Navbar />
+      <Cercle />
+      <MusiqueBar />
+
+    </div>
+  );
+};
+
+export default Home;
