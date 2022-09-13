5802f8317f520512e8c6486216a0e83c374ad34d
Lucas Heymans <lucas.heymans8@gmail.com>
2022-09-13 15:36:45 +0200

    Musique Bar Boutons

--- src/components/musique-bar/musique-bar.jsx
+++ src/components/musique-bar/musique-bar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import style from './musique-bar.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faForwardStep, faBackwardStep } from '@fortawesome/free-solid-svg-icons'
+import { faPlay, faForwardStep, faBackwardStep, faRepeat, faShuffle } from '@fortawesome/free-solid-svg-icons'
+
 const MusiqueBar = () => {
     return (
         <div className={style.musiqueBarGlobal}>
             <div className={style.musiqueBar}>
+                <FontAwesomeIcon icon={faShuffle} className={style.iconShuffle} />
                 <FontAwesomeIcon icon={faBackwardStep} className={style.iconBackward} />
                 <FontAwesomeIcon icon={faPlay} className={style.iconPlay} />
                 <FontAwesomeIcon icon={faForwardStep} className={style.iconForward} />
+                <FontAwesomeIcon icon={faRepeat} className={style.iconRepeat} />
             </div>
         </div>
     );
