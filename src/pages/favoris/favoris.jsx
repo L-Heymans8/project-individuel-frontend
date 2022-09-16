ac93234863045090b2eb94875d29ef52bba428a5
Lucas Heymans <lucas.heymans8@gmail.com>
2022-09-16 18:31:35 +0200

    PLaylist page

--- src/pages/favoris/favoris.jsx
+++ src/pages/favoris/favoris.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Navbar from "../../components/navbar/navbar";
-import MusiqueBar from "../../components/musique-bar/musique-bar";
 import LoginRegister from '../../components/login-register/login-register';
-import FavorisItem from '../../components/favoris/favorisItem';
+
 import style from './favoris.module.css'
+import FavorisItem from '../../components/favorisItem/favorisItem';
 
 const Favoris = () => {
     return (
         <div>
             <Navbar />
-            {/* <MusiqueBar /> */}
+
             <LoginRegister />
             <h1 className={style.favorisH1}>Favoris</h1>
             <FavorisItem />
--- src/pages/parametre/parametre.jsx
+++ src/pages/parametre/parametre.jsx
@@ -3,6 +3,8 @@ import Navbar from "../../components/navbar/navbar";
 import MusiqueBar from "../../components/musique-bar/musique-bar";
 import LoginRegister from '../../components/login-register/login-register';
 import style from './parametre.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHammer } from '@fortawesome/free-solid-svg-icons';
 
 const Parametres = () => {
     return (
@@ -10,6 +12,7 @@ const Parametres = () => {
             <Navbar />
             {/* <MusiqueBar /> */}
             <h1 className={style.parametreH1}>Paramètres</h1>
+            <p className={style.parametreP}><FontAwesomeIcon icon={faHammer} className={style.hammer} /> En construction <FontAwesomeIcon icon={faHammer} className={style.hammer} />  </p>
 
             <LoginRegister />
         </div>
